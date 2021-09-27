1e26d017c79bc71a8c29a1f99b7ad8523cd9b129
Elanis <elanis@hotmail.com>
2021-09-27 18:52:17 +0200

    Customized minimal gamemode

---
+++ code/ui/hud/Hud.cs
@@ -0,0 +1,11 @@
+﻿using Sandbox.UI;
+
+namespace SciFiPack.UI {
+	public partial class Hud : Sandbox.HudEntity<RootPanel> {
+		public Hud() {
+			if (IsClient) {
+				RootPanel.SetTemplate("/ui/hud/hud.html");
+			}
+		}
+	}
+}
